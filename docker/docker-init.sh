3259c052274814a3efe0dc2441059a9094f0bd53
AnhMTV <anhmtv@newayict.com>
2021-05-15 12:48:01 +0700

    #Điều chỉnh lại cơ chế sử dụng custom seting + bổ sung thêm một số tiện ích với jinja

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -65,15 +65,15 @@ echo_step "3" "Starting" "Setting up roles and perms"
 superset init
 echo_step "3" "Complete" "Setting up roles and perms"
 
-if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
-    # Load some data to play with
-    echo_step "4" "Starting" "Loading examples"
-    # If Cypress run which consumes superset_test_config – load required data for tests
-    if [ "$CYPRESS_CONFIG" == "true" ]; then
-        superset load_test_users
-        superset load_examples --load-test-data
-    else
-        superset load_examples
-    fi
-    echo_step "4" "Complete" "Loading examples"
-fi
+#if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
+#    # Load some data to play with
+#    echo_step "4" "Starting" "Loading examples"
+#    # If Cypress run which consumes superset_test_config – load required data for tests
+#    if [ "$CYPRESS_CONFIG" == "true" ]; then
+#        superset load_test_users
+#        superset load_examples --load-test-data
+#    else
+#        superset load_examples
+#    fi
+#    echo_step "4" "Complete" "Loading examples"
+#fi
